f8c2c9e2dc184b13bf7364192534d14c952b995d
kreichjr <kreichjr@gmail.com>
2022-08-02 21:36:37 -0500

    Updated code to the correct game and game logic.

--- game.py
+++ game.py
@@ -1,13 +1,15 @@
 import modules.vuerencards as Cards
 
-def check_guess(card, guess):
-	if card.value > guess:
-		print('You guessed too low!')
-		return False
-	if card.value < guess:
-		print('You guessed too high')
-		return False
-	print('You got it!')
+def check_guess(card, card2, guess):
+	delta = card.value - card2.value
+	if (
+			(delta < 0 and guess == 'higher') or 
+			(delta == 0 and guess == 'same') or 
+			(delta > 0 and guess == 'lower')):
+		print(f"You guessed correctly! The first card was the {card.display_name} and the second was the {card2.display_name} ")
+	else:
+		print(f"You guessed wrong. The first card was the {card.display_name} and the second was the {card2.display_name} ")
+
 	return True
 
 def main():
@@ -16,22 +18,23 @@ def main():
 	
 	while still_playing:
 		current_card = deck.draw_card()
+		following_card = deck.draw_card()
 		guessed = False
 		player_guess = ''
-		valid_guesses = [str(num) for num in range(1,14)]
+		valid_guesses = ['higher', 'lower', 'same']
 
 		while not guessed:	
-			player_guess = input('\nPlease guess the value of the drawn card! (1-13) ')
+			player_guess = input(f'\nThe current card is {current_card.display_name}. Will the next card be higher, lower, or the same value? ("higher", "lower", "same") ')
 			
 			if player_guess in valid_guesses:
-				guessed = check_guess(current_card, int(player_guess))
+				guessed = check_guess(current_card, following_card, player_guess)
 			else:
-				print('\nThat was an invalid guess. Please enter a value of 1 through 13.\n')
+				print('\nThat was an invalid guess. Please enter "higher", "lower", or "same".\n')
 
 		answer = ''
 		while answer.lower() != 'y' and answer.lower() != 'n':
 			
-			answer = input('\nDo you want to keep playing? y/N  ')
+			answer = input('\nDo you want to keep playing? (y/n)  ')
 
 			if answer.lower() == 'y':
 				print(f'Remaining cards in the deck: {len(deck)}')
